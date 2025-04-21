68c9c81e97a704efe26ed08818fc85fc7c8697dc
SakeenaNavavi <Sakeenanavavi@gmail.com>
2025-04-21 15:42:41 +0530

    Refactor Project Structure

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import StudentLandingPage from './Pages/StudentLandingPage/StudentLandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StudentLandingPage />
   </StrictMode>,
 )
