c30ec897c49527ec2c20f3beda2f14acbfec4071
SakeenaNavavi <Sakeenanavavi@gmail.com>
2025-04-21 16:33:40 +0530

    root file fix

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import StudentLandingPage from './Pages/StudentLandingPage/StudentLandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StudentLandingPage />
+    <App />
   </StrictMode>,
 )
