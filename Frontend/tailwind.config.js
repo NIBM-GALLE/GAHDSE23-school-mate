85088854a3b76e1411b94eb36aaf6d0be27ca8e8
SakeenaNavavi <Sakeenanavavi@gmail.com>
2025-04-22 20:29:56 +0530

    Student Login page completed

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3b306c",
+        secondary: "#87819d",
+        light: "#f2fbf8",
+      },
+    },
   },
   plugins: [],
 }
