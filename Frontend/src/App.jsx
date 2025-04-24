b2393bd098da767c696dd00dc3f0f90ab3d9a289
SakeenaNavavi <Sakeenanavavi@gmail.com>
2025-04-24 23:37:02 +0530

    adminportal completed -1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import StudentLogin from "./Pages/StudentLandingPage/StudentLogin";
 import TeacherLogin from "./Pages/TeacherLandingPage/TeacherLogin";
+import AdminLoginPage from "./Pages/AdminPortal/AdminPortal";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<StudentLogin />} />
         <Route path="/teacher-login" element={<TeacherLogin />} />
+        <Route path="/admin-login" element={<AdminLoginPage />} />
 
       </Routes>
     </BrowserRouter>
