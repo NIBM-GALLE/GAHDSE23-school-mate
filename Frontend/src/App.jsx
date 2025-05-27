d4e088520509352f348af0f2a437960e9ae8eb1e
PramoW22 <pramodhyamaddekanda@gmail.com>
2025-05-27 17:41:07 +0530

    routing configured

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import StudyMaterial from "./Pages/StudyMaterial";
-=======
 import StudentLogin from "./Pages/StudentLandingPage/StudentLogin";
 import TeacherLogin from "./Pages/TeacherLandingPage/TeacherLogin";
 import AdminLoginPage from "./Pages/AdminPortal/AdminPortal";
@@ -12,17 +9,13 @@ import PublishExamSchedule from "./Pages/PublishExamSchedule/PublishExamSchedule
 import TimeTableManager from "./Pages/TimeTableManager/TimeTableManager";
 import AttendanceManager from "./Pages/AttendanceManager/AttendanceManager";
 import AnnouncementPosting from "./Pages/AnnouncementPosting/AnnouncementPosting";
-
->>>>>>> 63834f1de3bb435132818742a1d6d3bcb37fc6a2
+import StudyMaterial from "./Pages/StudyMaterial";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<Navigate to="/study-material" />} />
-        <Route path="/study-material" element={<StudyMaterial />} />
-=======
+
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<StudentLogin />} />
         <Route path="/teacher-login" element={<TeacherLogin />} />
@@ -33,11 +26,9 @@ function App() {
         <Route path="/timetable-manager" element={<TimeTableManager />} />
         <Route path="/attendance-manager" element={<AttendanceManager />} />
         <Route path="/announcement_posting" element={<AnnouncementPosting />} />
+        <Route path="/study-material" element={<StudyMaterial />} />
 
         {/* Add more routes as needed */}
-
-
->>>>>>> 63834f1de3bb435132818742a1d6d3bcb37fc6a2
       </Routes>
     </BrowserRouter>
   );
